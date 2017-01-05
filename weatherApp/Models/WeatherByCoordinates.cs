5be91aa7d6533ba57b829dfad8022b571ac599c1
Nima <Nima@HP-MINI>
2017-01-05 11:59:39 +0100

    trigger both trip service and weather by cordinates

--- weatherApp/Models/WeatherByCoordinates.cs
+++ weatherApp/Models/WeatherByCoordinates.cs
@@ -15,7 +15,7 @@ namespace RejseplanenWeather.Models
         public object getWeatherForcast()
         {
             //string destination = "Roskilde";
-            var  url = string.Format("http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&APPID=67747486e2d5351d2a8c6c0c70cf5df7&units=metric", x,y);
+            var  url = string.Format("http://api.openweathermap.org/data/2.5/weather?lon={0}&lat={1}&APPID=67747486e2d5351d2a8c6c0c70cf5df7&units=metric", x,y);
             var client = new WebClient();
             var content = client.DownloadString(url);
             var serializer = new JavaScriptSerializer();
--- weatherApp/MyClient.js
+++ weatherApp/MyClient.js
@@ -2,11 +2,11 @@
 App.controller('TripController', function ($scope, $http) {
 
  $scope.originQuery = function (originString) {
-        return $http.get('/Location/OriginLocationService?originString=' + originString)
-          .success(function (originData) {
-              $scope.origin = originData;
-              console.log($scope.origin);
-          })
+     return $http.get('/Location/OriginLocationService?originString=' + originString)
+       .success(function (originData) {
+           $scope.origin = originData;
+           console.log($scope.origin);
+       });
     };
 
     $scope.destinationQuery = function (destinationString) {
@@ -14,15 +14,15 @@ App.controller('TripController', function ($scope, $http) {
           .success(function (destinationData) {
               $scope.destination = destinationData;
               console.log($scope.destination);
-          })
+          });
     };
 
     $scope.userQuery = function (originId, destinationId) {
-        return $http.get('/Trip/GetTrip?origin='+ originId+'&destination='+ destinationId)
+        return $http.get('/Trip/GetTrip?origin=' + originId + '&destination=' + destinationId)
           .success(function (tripData1) {
               $scope.trips1 = tripData1;
               console.log($scope.trips1);
-          })
+          });
     };
 
     $scope.weatherQueryByCoordinates = function (x,y) {
@@ -30,23 +30,23 @@ App.controller('TripController', function ($scope, $http) {
           .success(function (wDataByCoord) {
               $scope.wDataByCoord = wDataByCoord;
               console.log($scope.wDataByCoord);
-          })
+          });
     };
 
     $scope.weatherQuery = function (wDest) {
-        return $http.get('/Weather/GetWeather?wDestination='+ wDest)
+        return $http.get('/Weather/GetWeather?wDestination=' + wDest)
           .success(function (wData) {
               $scope.data1 = wData;
               console.log($scope.data1);
-          })
+          });
     };
 
     $scope.recommendationQuery = function (weatherDescription, temp, wind) {
-        return $http.post('/Recommendation/recommendation?weatherDescription=' + weatherDescription +"&temp="+temp+"&wind="+wind)
+        return $http.post('/Recommendation/recommendation?weatherDescription=' + weatherDescription + "&temp=" + temp + "&wind=" + wind)
           .success(function (rData) {
               $scope.data2 = rData;
               console.log($scope.data2);
-          })
+          });
     };
 });
 
